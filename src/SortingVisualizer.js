67d3e26b48445c998ba4d72d3734b6843e6b41ac
bajaj99prashant <prashantbajaj99@gmail.com>
2020-10-01 04:47:30 +0530

    added merge sort algorithm

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './sortingVisualizer.css';
+import * as sortingAlgorithms from './sorting-algorithms/sortingAlgorithms.js';
 
 export default class SortingVisualizer extends Component {
     constructor(props){
         super(props);
-        this.state({
 
-        });
+        this.state = {
+            array: [],
+        }
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray = () => {
+        const array = [];
+        const number = (89*(window.innerWidth))/400;
+        for (let i = 0; i < number; i++) {
+            array.push(randomFromInterval(5, 100));
+        }
+        this.setState({array});
+    }
+    mergeSort = () => {
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+        
     }
     render() {
+        const {array} = this.state;
+
         return (
-            <div>
-                
-            </div>
-        );
+            <div className="sorting-container">
+                {array.map((value, idx) => {
+                    return (<div className="sorting-bar" key={idx} style={{height: `${value}%`}}></div>);
+                })} 
+                <button className="array-generate" onClick={this.resetArray}>Generate new Array</button>
+                <button className="array-generate" onClick={this.mergeSort}>Merge Sort</button>
+            </div>)
+        ;
     }
+}
+
+function randomFromInterval(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
 }
